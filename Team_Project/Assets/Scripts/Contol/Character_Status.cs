a9df4c2c769302a5e129d0905675038ba4fd0bc4
unknown <leo929@naver.com>
2022-07-12 16:43:51 +0900

    Make control script.

---
+++ Team_Project/Assets/Scripts/Contol/Character_Status.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Status : MonoBehaviour
+{
+    public int HP = 100;
+    public int Max_HP = 100;
+    public int Stamina = 100;
+    public int Max_Stamina = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
